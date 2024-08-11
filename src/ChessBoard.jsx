5b3cb94558449713ac76bf6b6b92c4af86faf52a
Ahmed <ahmed_l2@yahoo.com>
2024-08-11 22:59:45 +0300

    Fixed UI buttons functionality

--- src/ChessBoard.jsx
+++ src/ChessBoard.jsx
@@ -10,7 +10,7 @@ import './App.css'
 
 
 const ChessBoard = ({ fenList , details}) => {
-  
+
 
 
   const [guess, setGuess] = useState('');
@@ -33,13 +33,13 @@ const ChessBoard = ({ fenList , details}) => {
     setGuess(e.target.value);//#1dd1a1
     if (e.target.value){
       document.getElementById('sub_guess').style='background-color:#10ac84;color:white'
-   
+
     }else{
 
       document.getElementById('sub_guess').style='background-color:#4d4d4d;color:silver;'
-    
+
     }
-    
+
   };
 
   if (currentIndex == fenList.length -1){
@@ -64,13 +64,13 @@ const ChessBoard = ({ fenList , details}) => {
     new Audio("./sfx/click.mp3").play();
     if (!guess){
       //#1dd1a1 btn color freen
-      
+
     }
     else{
       const  user_guess = guessed_inp.value;
-    
+
                 if (user_guess.trim() !== "") {
-                    
+
                     guessed_inp.disabled = true;
                     document.getElementById('sub_guess').disabled = true;
                     guessed_inp.style='cursor:not-allowed;background-color: #4d4d4d;'
@@ -91,7 +91,7 @@ const ChessBoard = ({ fenList , details}) => {
                     else if(result == '1/2-1/2'){
 
                         g_res.innerHTML=`Draw`;}
-                        
+
                     next_game.style = 'color:white;background-color:#e58e26';
 
 
@@ -129,7 +129,7 @@ const ChessBoard = ({ fenList , details}) => {
                     document.getElementById("next_game").style='color:white;background-color:#e58e26';
 
     }
-    
+
   }
 
   const next_game = () =>{
@@ -137,20 +137,18 @@ const ChessBoard = ({ fenList , details}) => {
   }
 
   const incrementIndex = () => {
-    new Audio("./sfx/move-self.mp3").play();
-
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % fenList.length);
- 
+    if (currentIndex !== fenList.length - 1) {
+        new Audio("./sfx/move-self.mp3").play();
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % fenList.length);
+    }
   };
 
 
-
-
-
   const decrementIndex = () => {
-    new Audio("./sfx/move-self.mp3").play();
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + fenList.length) % fenList.length);
-  
+    if (currentIndex !== 0) {
+      new Audio("./sfx/move-self.mp3").play();
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + fenList.length) % fenList.length);
+    }
   };
 
   const goToFirst = () => {
@@ -162,18 +160,18 @@ const ChessBoard = ({ fenList , details}) => {
     new Audio("./sfx/click.mp3").play();
     setCurrentIndex(fenList.length - 1);
   };
-  
-  
+
+
 
 
 
   return (
-   
+
     <div >
 
- 
+
       <div className="box" id="box-b">
-    
+
         <div className="board-container">
         <Chessground
         className="board"
@@ -181,7 +179,7 @@ const ChessBoard = ({ fenList , details}) => {
         width={750}
         height={750}
         KeyBindingComponent="true"
-        
+
 
 
         config={{ fen: fenList[currentIndex] ,
@@ -197,54 +195,54 @@ const ChessBoard = ({ fenList , details}) => {
       />
         </div>
         <div className="info-panel">
-          
+
             <p id="g_type">{event}</p>
 
             <div className="ratings-conta">
-                
+
                 <p id="w_elo"></p>
                 <p id="b_elo"></p>
-                
+
         </div>
             <p id="g_res">Game result</p>
-            
+
 
         <p id="g_avrg">Game average rating</p>
         <p id="result_info"></p>
 
             <div className="gte_sub_conta" id="ma_div">
-             
-                <input 
+
+                <input
                     type="number"
                     id="guess_inp"
                     placeholder="Enter Your Guess..."
                     required
                     value={guess}
                     onChange={handleInputChange}
-                   
+
                    />
                 <button id="sub_guess" onClick={sub_func} type="button">Submit</button>
-                
+
             </div>
 
             <div className="btn-container">
-           
+
                 <button id="reset-move" onClick={goToFirst}  className="move-btn">❮❮</button>
-               
+
                 <button id="prev-move" onClick={decrementIndex} className="move-btn">❮</button>
-         
+
                 <button id="next-move" onClick={incrementIndex} className="move-btn">❯</button>
                 <button id="skip-move"  onClick={goToLast} className="move-btn">❯❯</button>
             </div>
 
             <button id="next_game" onClick={next_game}>Next Game</button>
         </div>
-       
+
     </div>
-     
-    
+
+
     </div>
-    
+
   );
 };
 
