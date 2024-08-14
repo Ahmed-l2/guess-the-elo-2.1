384a0a18eeec3d86d158be57d10ae715ce052544
ABNSDEV <abnsmusic17@gmail.com>
2024-08-14 16:07:18 +0100

    logo add

--- src/NavBAr.jsx
+++ src/NavBAr.jsx
@@ -6,7 +6,7 @@ function Navbar() {
 
     return (
         <div className="nav">
-        
+        <img src='/dist/assets/favico.png' width={40}></img> 
        <p className="gte_logo" >WHAT'S THE ELO<span id='beta_txt'>BETA</span> <span id='vers_txt'> v1.1</span></p>
         
         <a className="kick_link" href="https://kick.com/darcss"  target="_blank" rel="noopener noreferrer"><img  width="30" src="https://img.freepik.com/premium-vector/kick-logo-vector-download-kick-streaming-icon-logo-vector-eps_691560-10814.jpg?w=826"></img></a>
