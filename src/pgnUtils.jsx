4d15ea63d446cbd0db1d50d64daf899c6dfd8a73
Ahmed <ahmed_l2@yahoo.com>
2024-08-16 03:30:36 +0300

    Added more PGN handling functionality

--- src/pgnUtils.jsx
+++ src/pgnUtils.jsx
@@ -1,22 +1,41 @@
 // Functions to extract match details from PGN
 export const extractMatchDetails = (pgn) => {
-    const event = pgn.match(/\[Event "(.*?)"(?: https?:\/\/\S*)?\]/)?.[1].split(' https://')[0].split('http://')[0] || null;
 
-   
+    const timeControlDict = {
+        "900": "Rapid",
+        "60": "Bullet",
+        "180": "Blitz"
+    };
+
+    function getGameTypeName(timeControl) {
+        return timeControlDict[timeControl] || timeControl;
+    }
+
+
     const whiteElo = pgn.match(/\[WhiteElo "(\d+)"\]/)?.[1] || null;
     const blackElo = pgn.match(/\[BlackElo "(\d+)"\]/)?.[1] || null;
     const result = pgn.match(/\[Result "(.*?)"\]/)?.[1] || null;
-    const opening = pgn.match(/\[Opening "(.*?)"\]/)?.[1] || null;
-    const termination =pgn.match(/\[Termination "(.*?)"\]/)?.[1];
+
+    const openingMatch = pgn.match(/\[ECOUrl\s+".*\/([^"\d\.]+)/);
+    const opening = openingMatch ? openingMatch[1].replace(/-/g, ' ') : null;
+
+    const timeControlMatch = pgn.match(/\[TimeControl\s*"\s*([^+"]+)/);
+    const timeControl = timeControlMatch ? timeControlMatch[1].trim() : null;
+    if (timeControl) {
+        const event = getGameTypeName(timeControl);
+        console.log(event);
+    }
+
+    const terminationMatch = pgn.match(/\[Termination\s+"[^"]*(won.*)"\]/);
+    const termination = terminationMatch ? terminationMatch[1].replace(/^.*?won\s/, 'won ') : null;
 
     return {
-      event,
-      whiteElo: whiteElo ? parseInt(whiteElo, 10) : null,
-      blackElo: blackElo ? parseInt(blackElo, 10) : null,
-      result,
-      opening,
-      termination
+        event,
+        whiteElo: whiteElo ? parseInt(whiteElo, 10) : null,
+        blackElo: blackElo ? parseInt(blackElo, 10) : null,
+        result,
+        opening,
+        timeControl,
+        termination
     };
-
-    
-  };
+};
