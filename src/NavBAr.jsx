191c8c6041a53ed7a073340fe2b22c0d4f9f4c2f
ABNSDEV <abnsmusic17@gmail.com>
2024-08-21 03:05:47 +0100

    responsivness media added

--- src/NavBAr.jsx
+++ src/NavBAr.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     return (
         <div className="nav">
         <img id='logo_img' src='/logo.png' width={40}/> 
-       <p className="gte_logo" >WHAT'S THE ELO<span id='beta_txt'>BETA</span> <span id='vers_txt'> v1.1</span></p>
+       <p className="gte_logo" >WHAT'S THE ELO<span id='beta_txt'>BETA</span> <span id='vers_txt'> v2.0</span></p>
         
         <a className="kick_link" href="https://kick.com/darcss"  target="_blank" rel="noopener noreferrer"><img  width="80" src="https://static.wikia.nocookie.net/logopedia/images/e/e6/Kick_%28Print%29.svg"></img></a>
       
